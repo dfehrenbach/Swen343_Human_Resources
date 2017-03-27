dd0483c2bcfa29a19cc9f5b0ecbc5da28b80a256
Sam Friedman <sfried8@gmail.com>
2017-03-27 13:18:06 -0400

    Added index.html page with forms for all API calls

--- hr/app.py
+++ hr/app.py
@@ -1,5 +1,6 @@
 '''Run of the HR application '''
 import connexion
+from flask import send_file
 # from flask_cors import CORS
 
 # Create connexion app and add the HR API
@@ -24,9 +25,11 @@ CORS(
 '''
 # Expose application var for WSGI support
 application = app.app
+@app.route('/')
+def index():
+    return send_file('html/index.html')
 
 if __name__ == '__main__':
     app.run(
-        port=8080,
-        debug=True
+        port=8080
     )
