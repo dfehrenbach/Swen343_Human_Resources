79ffdcada0a586fa83b01f7e73c8e3f5291bc523
Sam Friedman <sfried8@gmail.com>
2017-03-27 17:54:06 -0400

    added txt file with dummy employee data

--- hr/controllers/employees.py
+++ hr/controllers/employees.py
@@ -1,5 +1,12 @@
+import json
+import os
 def get(employee_id=None):
-    return {'Magic': 'Im magic for get', 'employee_id': employee_id}
+    scriptdir = '/'.join(os.path.dirname(os.path.abspath(__file__)).split('/')[:-2])
+    sp_file = os.path.join(scriptdir, 'static/dummy.txt')
+    obj = json.load(open(sp_file))
+    if employee_id != None and employee_id[0] < 3:
+        return obj["employee_array"][employee_id[0]-1]
+    return obj["employee_array"]
 
 
 def post(employee):
