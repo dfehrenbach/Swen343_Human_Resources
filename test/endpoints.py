d8134aaadbf08662aa2d068c758219189384c1f3
noesterle <noesterle1@gmail.com>
2017-04-18 21:42:01 -0400

    Endpoint Unit Testing. Making sure objects are retrieved correctly.

---
+++ test/endpoints.py
@@ -0,0 +1,96 @@
+import unittest
+# from unittest import mock
+# from unittest.mock import patch
+from controllers import employee, employees
+from hr.databasesetup import defaultInfo
+import hr.controllers
+import datetime
+import os
+
+
+class EndPointTests(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        defaultInfo()
+
+    # @patch("controllers.employee.get")
+    def test_getEmployee(self): #, get_employee):
+        mock_employee = {
+            'employee_array':
+                {
+                    'name': 'Joseph Campione',
+                    'team_start_date': datetime.date(2017, 1, 23),
+                    'is_active': True,
+                    'start_date': datetime.date(2017, 1, 23),
+                    'role': 'Developer',
+                    'department': 'Sales',
+                    'salary': '83860',
+                    'employee_id': 1,
+                    'birth_date': datetime.date(1992, 2, 12),
+                    'address': '0 Lomb Memorial Drive, Rochester, New York 14623'
+                }
+        }
+        retrieved_employee = employee.get(1)
+        print("RETRIEVED EMPLOYEE", retrieved_employee)
+        self.assertEqual(retrieved_employee['employee_array']['name'],mock_employee['employee_array']['name'])
+
+    def test_getEmployees(self):
+        retrieved_employee = employees.get([1])
+        mock_employee = {'employee_array':
+            [
+                {
+                    'name': 'Joseph Campione',
+                    'team_start_date': datetime.date(2017, 1, 23),
+                    'is_active': True,
+                    'start_date': datetime.date(2017, 1, 23),
+                    'role': 'Developer',
+                    'department': 'Sales',
+                    'salary': '83860',
+                    'employee_id': 1,
+                    'birth_date': datetime.date(1992, 2, 12),
+                    'address': '0 Lomb Memorial Drive, Rochester, New York 14623'
+                }
+            ]
+        }
+        print("RETRIEVED EMPLOYEE 1", retrieved_employee)
+        self.assertEqual(retrieved_employee['employee_array'][0]['name'],mock_employee['employee_array'][0]['name'])
+        retrieved_employees = employees.get([1,2])
+        mock_employees = {'employee_array':
+            [
+                {
+                    'name': 'Joseph Campione',
+                    'start_date': datetime.date(2017, 1, 23),
+                    'birth_date': datetime.date(1992, 2, 12),
+                    'employee_id': 1,
+                    'is_active': True,
+                    'address': '0 Lomb Memorial Drive, Rochester, New York 14623',
+                    'salary': '83860',
+                    'team_start_date': datetime.date(2017, 1, 23),
+                    'department': 'Sales',
+                    'role': 'Developer'
+                },
+                {
+                    'name': 'Matthew Chickering',
+                    'start_date': datetime.date(2017, 1, 23),
+                    'birth_date': datetime.date(1992, 2, 12),
+                    'employee_id': 2,
+                    'is_active': True,
+                    'address': '1 Lomb Memorial Drive, Rochester, New York 14623',
+                    'salary': '51943',
+                    'team_start_date': datetime.date(2017, 1, 23),
+                    'department': 'Manufacturing',
+                    'role': 'Developer'
+                }
+            ]
+        }
+        self.assertEqual(retrieved_employees['employee_array'][0]['name'],mock_employees['employee_array'][0]['name'])
+        self.assertEqual(retrieved_employees['employee_array'][1]['name'], mock_employees['employee_array'][1]['name'])
+
+        print("RETRIEVED EMPLOYEE 2", retrieved_employees)
+
+        #TODO get teardown to remove testDB.
+        @classmethod
+        def tearDownClass(cls):
+            os.remove("hr.sqlite3")
+            print("Things in current dir", os.listdir("."))
